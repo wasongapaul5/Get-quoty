b166c5c8c468b13d04967a25fa32821dbecf363d
wasongapaul5 <wasongapaul5@gmail.com>
2019-10-29 10:21:57 +0300

    add components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,13 +6,4 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  quotes:Quote = [
-    {id:1,name:'Life is too short to spend it alone'},
-    {id:2,name:'Life is short,so am I'},
-    {id:3,name:'Destroy what destroys you'},
-    {id:4,name:'Mistakes are proof that you are trying'},
-    {id:5,name:'Choose people who choose you'},
-    {id:6,name:'Be a voice,not an echo'},
-  ];
- 
 }
