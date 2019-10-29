e56ef9c127ae0722ff7cd8d4f03cfcab6fb71cb0
wasongapaul5 <wasongapaul5@gmail.com>
2019-10-29 10:26:01 +0300

    add toggle functionality

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -1,22 +1,36 @@
 import { Component, OnInit } from '@angular/core';
 import { Quote }from '../quote';
+
 @Component({
   selector: 'app-quote',
   templateUrl: './quote.component.html',
   styleUrls: ['./quote.component.css']
 })
+
 export class QuoteComponent implements OnInit {
 quotes:Quote [] = [
-new Quote(1,'Life is too short to spend it alone','Paul Oketch Wasonga'),
-new Quote(2,'Life is short,so am I','As much as life seems to be either hard or simple,you should take it as it is'),
-new Quote(3,'Destroy what destroys you','If something does not add a value to your life you should go against it'),
-new Quote(4,'Mistakes are proof that you are trying','Mistakes are a way to learning new things and it is right to make mistake'),
-new Quote(5,'Choose people who choose you','Be with the people who are willing to travel with you through the same journey of life'),
-new Quote(6,'Be a voice,not an echo','Always be someone who uses his/her words to say something useful'),
+new Quote(1,'Stop looking for happines where you lost it','Paul Tennison.', 'Paul Wasonga', 0,0,new Date(2017,3,14)),
+new Quote(2,'There is no elevator to success,you must take the stairs','Fredrick Sundqvist','Paul Wasonga', 0,0,new Date (2019,3,12)),
+new Quote(3,'Destroy what destroys you','Nina Eklund.','Paul Wasonga', 0,0,new Date(2016,4,17)), 
+new Quote(4,'Mistakes are proof that you are trying','Petter Ericsson.','Paul Wasonga', 0,0,new Date(2018,1,21)),
+new Quote(3,'Just because my path is different doest not mean I am lost','Glen Leibowitz.','Paul Wasonga', 0,0,new Date(2010,4,17)), 
+new Quote(4,'Be the change you want to see in this world','Martin Luther.','Paul Wasonga', 0,0,new Date(2018,1,21)),
 ];
 
 toggleDetails(index){
   this.quotes[index].showDescription = !this.quotes[index].showDescription;
+}
+completeQuote(isComplete,index){
+  if(isComplete){
+    this.quotes.splice(index,1);
+  }
+}
+
+addNewQuotes(quotes){
+  let quoteLength = this.quotes.length;
+  quotes.id = quoteLength + 1;
+  quotes.completeDate(quotes.completeDate)
+  this.quotes.push(quotes)
 }
   constructor() { }
 
