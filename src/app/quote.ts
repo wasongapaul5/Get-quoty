21e2c35905bc66394ea6f97198b0f3b7a492cfe4
wasongapaul5 <wasongapaul5@gmail.com>
2019-10-29 10:24:27 +0300

    add classes

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,6 +1,6 @@
 export class Quote {
-    showDescription:boolean;
-    constructor(public id:number,public name:string,public description:string){
+    public showDescription:boolean;
+    constructor(public id:number,public name:string, public description:string, public author:string,public upvotes:number,public downvotes:number, public completeDate:Date){
         this.showDescription=false;
     }
 }
