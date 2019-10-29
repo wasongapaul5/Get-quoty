4a21ae87af773d0220085516b6a34d58aac3af73
wasongapaul5 <wasongapaul5@gmail.com>
2019-10-29 10:22:31 +0300

    import form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from'@angular/forms';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { QuoteComponent } from './quote/quote.component';
 import { QuoteDetailComponent } from './quote-detail/quote-detail.component';
+import { DateCountPipe } from './date-count.pipe';
+import { QuoteFormComponent } from './quote-form/quote-form.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { VotesComponent } from './votes/votes.component';
+import { AngularFontAwesomeModule } from 'angular-font-awesome';
+import { CountInputComponent } from './count-input/count-input.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     QuoteComponent,
-    QuoteDetailComponent
+    QuoteDetailComponent,
+    DateCountPipe,
+    QuoteFormComponent,
+    VotesComponent,
+    CountInputComponent,
   ],
+  
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    BrowserAnimationsModule,
+    AngularFontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
